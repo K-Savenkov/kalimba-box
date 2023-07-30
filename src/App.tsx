e6363cf55f94c6e19398c30add3a1ca45a4741b8
Konstantin Savenkov <ksavenkov@yahoo.com>
2023-07-31 00:13:51 +0200

    add third row of listing keys on keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,12 @@ function App() {
     <KalimbaKeys />
     : <div className='intro'>
       <p>
-        This site will play kalimba sounds on hover cursor <br />and/or by pressing "q-p" 'a-l' keyboard layout<br />
+        This site will play kalimba sounds on hover cursor <br />and/or by pressing <br />
+
+        "<kbd>q w e r t y ... p</kbd>"<br />
+        "<kbd>a s d ... ;"</kbd><br />
+        "<kbd>z x c ... /" </kbd><br />
+        Keyboard layout
       </p>
       <button type="button" onClick={() => { setIsKalimbaOn(true) }}>Click HERE turn on kalimba keyboard</button>
     </div>
